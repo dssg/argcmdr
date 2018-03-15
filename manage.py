ed11f2e9de2d253acf84f177c5aaf927f1a66d1d
Jesse London <jesselondon@gmail.com>
2018-03-15 17:30:29 -0500

    added management command for bumpversion and optionally a full release

--- manage.py
+++ manage.py
@@ -1,4 +1,7 @@
 """argcmdr's own management module"""
+import copy
+import re
+
 from argcmdr import Local
 
 
@@ -10,6 +13,49 @@ class Manage(Local):
 
         def prepare(self):
             return self.local['tox']
+
+    class Bump(Local):
+        """bump package version (and optionally build and release)"""
+
+        def __init__(self, parser):
+            parser.add_argument(
+                'part',
+                choices=('major', 'minor', 'patch'),
+                help="part of the version to be bumped",
+            )
+            parser.add_argument(
+                '-b', '--build',
+                action='store_true',
+                help='build the new version',
+            )
+            parser.add_argument(
+                '-r', '--release',
+                action='store_true',
+                help='release the new build',
+            )
+
+        def prepare(self, args, parser):
+            (_code,
+             stdout,
+             _err) = yield self.local['bumpversion']['--list', args.part]
+
+            if args.build:
+                yield self.root['build'].prepare()
+
+                if args.release:
+                    rel_args = copy.copy(args)
+                    if stdout is None:
+                        rel_args.version = ('DRY-RUN',)
+                    else:
+                        (version_match,) = re.finditer(
+                            r'^new_version=([\d.]+)$',
+                            stdout,
+                            re.M,
+                        )
+                        rel_args.version = version_match.groups()
+                    yield self.root['release'].prepare(rel_args)
+            elif args.release:
+                parser.error('will not release package without build')
 
     class Build(Local):
         """build package"""
